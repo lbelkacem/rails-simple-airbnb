d0cb09b4ce0f7670e14e92109c2cb847990a8bc8
Lamia Belkacem <lamia.belkacem86@gmail.com>
2022-11-23 16:51:16 -0500

    model controller seed index show new create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Wondeful Flat Montreal',
+  address: '10 sherbrook st Montreal H1X 1DT',
+  description: 'A wonderful and gorgeous flat with a big pool. Thre bedrooms, spacisous living area, well equiped kitchen and a beautiful garden',
+  price_per_night: 95,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Lovely Flat Paris',
+  address: '7 Duguay Trouin St Paris 75006',
+  description: 'A lovely flat to discover Paris. one bedroom, modern living room, an open kitchen and a balcony with a view to the eifeil tower',
+  price_per_night: 90,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Modern Flat New York',
+  address: '5 Avenue New York Y8 1NT',
+  description: 'A modern flat near all accomodations. Two bedrooms, open living area and a cute little kitchen',
+  price_per_night: 105,
+  number_of_guests: 3
+)
